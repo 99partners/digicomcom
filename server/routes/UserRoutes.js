c1c07316c029162c8636b8dddc8aaa9c9c4410b7
Mansi <mansigangani126@gmail.com.com>
2025-07-02 17:54:03 +0530

    redirection done

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -2,20 +2,8 @@ import express from 'express';
 import userAuth from '../middleware/userAuth.js';
 import { getUserData } from '../controllers/UserController.js';
 
-
 const UserRouter = express.Router()
 
-
-
-
 UserRouter.get('/data', userAuth, getUserData)
-
- 
-
-
-
-
-
-
 
 export default UserRouter;
