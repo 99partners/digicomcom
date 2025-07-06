1b7629a52a1881e8e27159af52a6e0a9bb3cf30f
Mansi <mansigangani126@gmail.com.com>
2025-07-06 15:29:21 +0530

    32

--- client/src/config/api.config.js
+++ client/src/config/api.config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Ensure HTTPS is always used in production
 export const API_BASE_URL = process.env.NODE_ENV === 'production' 
   ? 'https://api.99digicom.com'
-  : 'https://99digicom.com/';
+  : '';
 
 // Configure axios defaults
 const axiosInstance = axios.create({
