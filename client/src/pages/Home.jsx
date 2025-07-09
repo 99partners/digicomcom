1ae2828127e250a79f963758b7d1d2937ac0def6
Anuj Patel <patel.anujp0101@gmail.com>
2025-07-09 12:14:25 +0530

    home updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -109,15 +109,14 @@ const Home = () => {
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-20 md:py-24 lg:py-32 text-center">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4 sm:mb-6 md:mb-8 leading-tight text-gray-900">
             Empowering Businesses 
-            <br /> in 
-            <br className="hidden sm:block" />
+            <br /> in the <br />
             <span className="bg-gradient-to-r from-green-700 to-green-400 text-transparent bg-clip-text animate-gradient">
               Digital Commerce
             </span>
           </h1>
-          {/* <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8 sm:mb-10 md:mb-12 text-gray-600 max-w-2xl sm:max-w-3xl lg:max-w-4xl mx-auto leading-relaxed">
-            Scale your business with seamless ONDC integration and strategic partnerships.
-          </p> */}
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8 sm:mb-10 md:mb-12 text-gray-600 max-w-2xl sm:max-w-3xl lg:max-w-4xl mx-auto leading-relaxed">
+            Setup, manage, advertise, and co-brand across all top marketplaces.
+          </p>
           <div className="flex justify-center">
             <Link
               to="/partnerlogin"
