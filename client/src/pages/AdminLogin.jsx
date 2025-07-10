4893863b3c35b4073cf0b7937e7af9e0abe74719
Mansi <mansigangani126@gmail.com.com>
2025-07-10 17:56:16 +0530

    99

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { AUTH_CONFIG } from '../config/auth.config';
+import AUTH_CONFIG from '../config/auth.config';
 import apiService from '../config/api.config';
 
 const AdminLogin = () => {
