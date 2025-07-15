3be28630538c194eda9072faca5df013b536f9d8
Trisha Rathod <trisharathod423@gmail.com>
2025-07-15 14:29:26 +0530

    1

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 export const AppContextProvider = ({ children }) => {
     axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_API_URL || "https://99digicom.com";
+    const backendUrl = import.meta.env.VITE_API_URL || "https://api.99digicom.com";
     if (!import.meta.env.VITE_API_URL) {
         console.warn("⚠️ Warning: VITE_API_URL is not defined in .env. Falling back to https://99digicom.com");
     }
