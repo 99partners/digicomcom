b9c95e50effa1a9915168cd53c63b46b4b5433dc
sunny kavar <kavarsunny51@gmail.com>
2025-07-17 10:23:20 +0530

    myapplication

--- server/models/PlatformAMSForm.js
+++ server/models/PlatformAMSForm.js
@@ -4,7 +4,7 @@ const platformAMSFormSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: false // Make userId optional for now
+    required: true
   },
   marketplaces: {
     ondc: { type: Boolean, default: false },
