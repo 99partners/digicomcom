72439798143f509ab02b1995592a465e4faff209
Trisha Rathod <trisharathod423@gmail.com>
2025-07-18 15:42:39 +0530

    index

--- server/index.js
+++ server/index.js
@@ -63,6 +63,7 @@ app.use((req, res, next) => {
     // Check if origin is allowed
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Vary', 'Origin');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
