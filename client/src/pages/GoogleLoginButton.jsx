fe7ecc8a6bfae3469d9c00ad2d6141beed1ff3e1
Mansi <mansigangani126@gmail.com.com>
2025-07-30 12:53:04 +0530

    route change

--- client/src/pages/GoogleLoginButton.jsx
+++ client/src/pages/GoogleLoginButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const GoogleLoginButton = () => {
   const handleGoogleLogin = () => {
-    window.location.href = `${import.meta.env.VITE_API_URL || 'http://localhost:5051'}/api/auth-google/google-login`;
+    window.location.href = `${import.meta.env.VITE_API_URL || 'http://localhost:5051'}/api/google/google-login`;
   };
 
   return (
