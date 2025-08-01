2ed65428fd2196e971d1e64dedaac9200cd3d6d8
Mansi <mansigangani126@gmail.com.com>
2025-08-01 17:36:49 +0530

    google login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AppContextProvider } from './context/AppContext';
 import './i18n'; // Import i18n configuration
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppContextProvider>
+    <GoogleOAuthProvider clientId='575870145123-e34ouiqri391vkgmopg0cusch68mtgiv.apps.googleusercontent.com'>
       <App />
-    </AppContextProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
